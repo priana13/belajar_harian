2435b52271dced3eede741a793d5a9f1e7475ca2
priana13 <herian.sap@gmail.com>
2025-02-22 20:50:19 +0700

    rapihkan layout mateir hari ini

--- resources/views/filament/pages/link-materi-harian.blade.php
+++ resources/views/filament/pages/link-materi-harian.blade.php
@@ -9,11 +9,23 @@
 
     </div>
 
-    <div class="rounded py-3 px-12">
+    <div class="rounded py-3">
         {{-- list materi hari ini --}}
-        <h2 class="text-xl font-bold my-2">Materi Hari ini</h2>
+        <div class="flex justify-between">
+            <h2 class="text-xl font-bold my-2">Materi Hari ini</h2>
 
-        <div class="w-full max-w-4xl">
+            {{-- <button class="text-center text-blue-600 flex disable">
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+                </svg>  
+                <span class="ms-1">Text</span>            
+                
+            </button> --}}
+
+        </div>
+
+        <div class="w-full">
             <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
                 <thead class="bg-sky-100">
                     <tr>
@@ -27,7 +39,7 @@
                             Judul Pertemuan
                         </th>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Actions
+                            Link
                         </th>
                     </tr>
                 </thead>
@@ -49,13 +61,13 @@
                             <td class="px-6 py-4 text-sm text-gray-500">
                                 {{ $row->materi_detail->judul }}
                             </td>
-                            <td class="px-6 py-4 text-sm font-medium space-x-2">
+                            <td class="px-6 py-4 text-sm font-medium">
                                 <a 
                                     href="{{ route('link_materi' , $row->code) }}"
                                     class="text-blue-600 hover:bg-blue-100 px-3 py-1 rounded-md transition-colors duration-200 inline-block"
                                     target="_blank"
                                 >
-                                    Lihat
+                                {{ route('link_materi' , $row->code) }}
                                 </a>
                                 {{-- <a 
                                     href="{{ route('link_materi' , $row->code) }}"
@@ -86,11 +98,11 @@
 
     @if(count($ujian_pekanan) > 0)
 
-    <div class="rounded py-3 px-12">
+    <div class="rounded py-3">
         {{-- list materi hari ini --}}
         <h2 class="text-xl font-bold my-2">Ujian Pekanan</h2>
 
-        <div class="w-full max-w-4xl">
+        <div class="w-full">
             <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
                 <thead class="bg-orange-100">
                     <tr>
@@ -150,11 +162,11 @@
 
     @if(count($ujian_akhir) > 0)
 
-    <div class="rounded py-3 px-12">
+    <div class="rounded py-3">
         {{-- list materi hari ini --}}
         <h2 class="text-xl font-bold my-2">Ujian Akhir</h2>
 
-        <div class="w-full max-w-4xl">
+        <div class="w-full">
             <table class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
                 <thead class="bg-indigo-100">
                     <tr>
