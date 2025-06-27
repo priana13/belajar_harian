f8189ecada8c0f1725e62a8d55853ab0a1cac849
priana13 <herian.sap@gmail.com>
2025-06-27 22:53:10 +0700

    w-full mobil

--- resources/views/livewire/materi-video/halaman-materi-video.blade.php
+++ resources/views/livewire/materi-video/halaman-materi-video.blade.php
@@ -1,14 +1,14 @@
 <div class="lg:flex min-h-screen" x-data="{ openSidebar: true }">
    
     <!-- Sidebar -->
-    <aside x-show="openSidebar" class="bg-white shadow-lg p-6 w-1/3 relative" :class="{ 'hidden': !openSidebar && window.innerWidth < 1024, 'lg:block': openSidebar }">
+    <aside x-show="openSidebar" class="bg-white shadow-lg p-6 w-full sm:w-1/3 relative" :class="{ 'hidden': !openSidebar && window.innerWidth < 1024, 'lg:block': openSidebar }">
       <button class="hidden lg:flex items-center justify-center absolute -right-3 top-1 w-10 h-10 bg-white border border-gray-200 shadow rounded-lg z-20 hover:bg-gray-100 transition p-3" @click="openSidebar = false" title="Sembunyikan Sidebar" style="line-height:1;">
        
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z"/></svg>
         
       </button>
       <div class="flex justify-between items-center mb-4 lg:hidden">
-        <h2 class="text-xl font-bold text-green-700">CODEPOLITAN</h2>
+        <h2 class="text-xl font-bold text-green-700">BISI</h2>
         <button class="text-sm text-red-500 font-semibold" @click="openSidebar = false">✖</button>
       </div>
       <h3 class="text-sm font-semibold text-gray-700 mb-4 hidden lg:block">MODULE BELAJAR</h3>
@@ -38,6 +38,7 @@
         </button>
       </div>
 
+      {{-- breadcrumb --}}
       <nav class="text-sm text-gray-500 mb-2 flex items-center gap-1">
         <span>{{ $materi->kategori->nama_kategori }}</span>
         
@@ -54,7 +55,7 @@
 
       <!-- Video -->
       <div class="aspect-w-16 aspect-h-9 mb-6 text-center p-4">
-        <iframe class="w-full min-h-screen rounded-lg shadow-lg" src="https://www.youtube.com/embed/36iA7VRW1ts" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe class="w-full h-full sm:min-h-screen rounded-lg shadow-lg" src="https://www.youtube.com/embed/36iA7VRW1ts" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     
 
