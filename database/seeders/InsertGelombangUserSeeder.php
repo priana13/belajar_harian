90eba3db84bb95d6a465d6adafb38d9385f4faab
priana13 <herian.sap@gmail.com>
2025-09-07 16:52:16 +0700

    untuk peserta yang belum punya gel

--- database/seeders/InsertGelombangUserSeeder.php
+++ database/seeders/InsertGelombangUserSeeder.php
@@ -17,8 +17,7 @@ class InsertGelombangUserSeeder extends Seeder
      */
     public function run()
     {
-        $list_gelombang = Gelombang::get();          
-
+        $list_gelombang = Gelombang::doesntHave('peserta')->get(); 
         
         foreach ($list_gelombang as $key => $gelombang) {             
             
@@ -53,12 +52,39 @@ class InsertGelombangUserSeeder extends Seeder
 
         $this->command->info("Peserta yang tersisa: $list_peserta");
 
-        // foreach ($list_peserta as $key => $peserta) {
-           
-        //     $angkatan = $peserta->angkatan[0];
-       
+        $lanjutkan = $this->command->ask("Apakah perlu dilanjutkan untuk sisa peserta nya?", true);
 
-        // }
+        if($lanjutkan){
 
+            $this->command->info("Mari kita eksekusi sisa peserta yang belum masuk ke gelombang manapun");
+
+            $list_peserta = User::where('gelombang_id' , 0)->whereHas('angkatan')->get();
+
+        
+            $total_ex = 0;
+
+            foreach ($list_peserta as $key => $peserta) {
+
+                $angkatan_user = $peserta->angkatan_user()->latest()->first();
+
+                if($angkatan_user) {
+
+                    $peserta->gelombang_id = $angkatan_user->angkatan->gelombang_id;
+                    $peserta->save();
+
+                    $total_ex ++;
+                }                   
+        
+
+            }
+
+            $list_peserta = User::where('gelombang_id' , 0)->whereHas('angkatan')->count();
+
+            $this->command->info("Ex: $total_ex sehingga Peserta yang tersisa tanpa gelombang: $list_peserta");
+
+
+        }
+
+    
     }
 }
