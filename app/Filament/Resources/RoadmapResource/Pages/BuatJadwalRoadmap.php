5b3e2f166b91701040ac88a85a9dc0e6780269bb
priana13 <herian.sap@gmail.com>
2025-10-06 10:23:54 +0700

    halaman reoadmap

---
+++ app/Filament/Resources/RoadmapResource/Pages/BuatJadwalRoadmap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\RoadmapResource\Pages;
+
+use App\Filament\Resources\RoadmapResource;
+use Filament\Resources\Pages\Page;
+
+class BuatJadwalRoadmap extends Page
+{
+    protected static string $resource = RoadmapResource::class;
+
+    protected static string $view = 'filament.resources.roadmap-resource.pages.buat-jadwal-roadmap';
+}
