13bc57349838dc01741a702040e05dcd9ce7052c
priana13 <herian.sap@gmail.com>
2025-10-18 11:45:45 +0700

    seeder sertifikat user

--- database/seeders/SertifikatUserSeeder.php
+++ database/seeders/SertifikatUserSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Materi;
+use App\Models\Sertifikat;
+use App\Models\AngkatanUser;
+use App\Models\SertifikatUser;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SertifikatUserSeeder extends Seeder
 {
@@ -16,7 +21,43 @@ class SertifikatUserSeeder extends Seeder
         // 1. Cari User yang memiliki sertifikat
         // 2. Untuk setiap user, buat entri di tabel SertifikatUser
 
-        
+        $angkatan_users = AngkatanUser::lulus()->get();
+
+        $jumlah = 0;
+
+        foreach ($angkatan_users as $row) {         
+
+            $user = User::find($row->user_id);
+            $materi = Materi::find($row->angkatan->materi_id);
+            $sertifikat = Sertifikat::find($row->angkatan->sertifikat_id);
+         
+            // Cek apakah user sudah memiliki sertifikat untuk materi ini
+            $existing_sertifikat = SertifikatUser::where('user_id', $user->id)
+                ->where('materi_id', $materi->id)
+                ->first();
+
+            if (!$existing_sertifikat) {
+                // Buat sertifikat baru
+                SertifikatUser::create([
+                    'user_id' => $user->id,
+                    'sertifikat_id' => $sertifikat->id, // Asumsikan ada sertifikat default dengan ID 1
+                    'materi_id' => $materi->id,
+                    'predikat' => $row->predikat, // Contoh predikat
+                    'tanggal' => $row->angkatan->tanggal_ujian,
+                    'code' => ($row->code) ? $row->code :uniqid(),
+                    'ttd_image' => 'img/ttd2.png',
+
+                    'ttd_nama' => 'Irfan Bahar Nurdin, S.Th.I, M.M.,',
+                    'ttd_jabatan' => 'Manager',
+                ]);
+
+                $jumlah++;
+            }
+        }
+
+        $this->command->info('Selesai membuat sertifikat untuk ' . $jumlah . ' peserta.');
+
+
 
 
         // Generate Daftar Nilai
