a6c5c506ddd0959f186f201fc0b14ac99ac06398
priana13 <herian.sap@gmail.com>
2026-04-27 16:39:33 +0700

    label kelas MMI

--- resources/views/daftar-kelas-khusus.blade.php
+++ resources/views/daftar-kelas-khusus.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="p-4 text-center">
-    <h1 class="text-2xl font-bold mb-4">Daftar Kelas RJMI</h1>
+    <h1 class="text-2xl font-bold mb-4">Daftar Kelas MMI</h1>
     <div class="space-y-4">
 
         <div class="p-4 bg-white rounded shadow">
@@ -25,7 +25,7 @@
                 @csrf
                 <input type="hidden" name="group_id" value="{{ $group->id }}">
 
-                <button type="submit" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Ikuti Kelas RJMI</button>
+                <button type="submit" class="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Ikuti Kelas</button>
             </form>
 
         </div>
