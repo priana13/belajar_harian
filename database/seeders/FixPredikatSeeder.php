89a5ccf7a732ba562dea8ec4910600a1cb8d9018
priana13 <herian.sap@gmail.com>
2026-06-02 10:49:07 +0700

    MATERI ID FIX PREDIKATE SEEDER

--- database/seeders/FixPredikatSeeder.php
+++ database/seeders/FixPredikatSeeder.php
@@ -4,25 +4,21 @@ namespace Database\Seeders;
 
 use App\Models\SertifikatUser;
 use App\Models\Ujian;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class FixPredikatSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        $materiId = env('MATERI_ID', 56);
 
-        $list_ujian = Ujian::where('materi_id' , 56)->where('jenis_ujian_id' , 3)->get();
-   
-        foreach($list_ujian as $ujian){
+        $list_ujian = Ujian::where('materi_id', $materiId)
+                           ->where('jenis_ujian_id', 3)
+                           ->get();
 
+        foreach ($list_ujian as $ujian) {
             $nilai = $ujian->nilai ?? 0;
-
-            $predikat = Ujian::tentukanPredikat($ujian->id, $nilai , 8);
-
+            $predikat = Ujian::tentukanPredikat($ujian->id, $nilai, 8);
             SertifikatUser::where('ujian_id', $ujian->id)->update(['predikat' => $predikat]);
         }
     }
